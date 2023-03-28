2b2c4ea9b51e1c0e75f134d4450089602add7959
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2023-03-28 01:39:52 -0500

    first commit

---
+++ app/Http/Livewire/Articles.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Article;
+use Livewire\Component;
+
+class Articles extends Component
+{
+    public $buscar_1 ="";
+    public function render()
+    {
+        return view('livewire.articles',[
+            'articles'=>Article::where('title','like',"{$this->buscar_1}%")->get()
+        ]);
+    }
+}
---
+++ resources/views/livewire/articles.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <h1> Lista de Articulos </h1>
+    {{-- <h2> Categoria : {{ $category }} </h2> --}}
+    {{-- <input wire:model = "category" type="text" placeholder="Categoria .."> --}}
+    {{-- <input wire:model.lazy = "category" type="text" placeholder="Categoria .."> --}}
+    {{-- <input wire:model.defer = "category" type="text" placeholder="Categoria .."> --}}
+    <input wire:model="buscar_1" type="search" placeholder="Buscar">  
+    <ul>
+        @foreach ($articles as $article)
+            <li> {{ $article->title }}
+        @endforeach
+    </ul>
+</div>
